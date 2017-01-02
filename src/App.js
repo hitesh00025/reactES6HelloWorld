037c37d6fef14e95ca6df31b7a59e921550f1da8
hitesh kumar <hitesh.kumar1@merck.com>
2017-01-01 19:59:38 -0500

    updated hello world message

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export default class App extends Component {
   render() {
     return (
-      <h1>Hello, world.</h1>
+      <h1>Hello, world REACT in ES6</h1>
     );
   }
 }
